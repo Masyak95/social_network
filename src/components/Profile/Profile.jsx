6ad6d29e91a7096d9861ac0c5555adb2fb67516a
Maryia <97767940+Masyak95@users.noreply.github.com>
2022-12-21 17:48:26 +0100

    css modules

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={s.content}>
         <div>
-            <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350' />
+            <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'  alt={"ava"}/>
         </div>
         <div>
             ava + description
@@ -13,16 +14,16 @@ const Profile = () => {
             <div>
                 New post
             </div>
-            <div>
-                <div>
+            <div className={s.posts}>
+                <div className={s.item}>
                     post 1
                 </div>
-                <div>
+                <div className='item'>
                     post 2
                 </div>
             </div>
         </div>
     </div>
-};
+}
 
 export default Profile;
