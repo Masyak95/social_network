40ac6364bbed23e338d4d45e7ecc5782d8dd34ac
Maryia <97767940+Masyak95@users.noreply.github.com>
2022-12-21 14:22:46 +0100

    profile, header, navbar, dialogs

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <header className='header'>
+                <img src='https://www.freelogodesign.org/Content/img/logo-ex-7.png' />
+            </header>
+        </div>
+    );
+};
+
+export default Header;
