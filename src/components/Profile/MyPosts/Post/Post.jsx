5a26aa49f8872ad9b64c9739e75bc51f46e2bffd
Maryia <97767940+Masyak95@users.noreply.github.com>
2022-12-21 18:06:54 +0100

    props for Post

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={s.item}>
             <img src='https://www.play.cz/wp-content/uploads/2019/04/Ava-Max.jpg' alt={"ava"}/>
-            post 1
+            {props.message}
             <div>
                 <span>like</span>
             </div>
         </div>
     )
-
 }
 
 export default Post;
